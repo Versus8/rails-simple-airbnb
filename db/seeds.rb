febfb178780b88b580841b0aa05e5e208de7e8c3
Veronique Tureau <veronique.tureau@gmail.com>
2023-10-19 21:33:31 -0400

    flat controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Sea and mountain',
+  address: '456 sea street',
+  description: 'You can do all the activities your heart wishes',
+  price_per_night: 43,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Night bubble',
+  address: '345 forest avenue',
+  description: 'A bubble of love in the forest',
+  price_per_night: 45,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Elegant flat in the center of New York',
+  address: '398a baker street',
+  description: 'You really are in New York',
+  price_per_night: 123,
+  number_of_guests: 5
+)
